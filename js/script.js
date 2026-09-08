7dc292c63b243d711942cb9957d6808bfa22867b
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2026-09-08 20:16:20 +0100

    Add function to close modal on window click in script.js file

--- js/script.js
+++ js/script.js
@@ -11,15 +11,28 @@ document.addEventListener("DOMContentLoaded", function () {
   const modal = document.getElementById("videoModal");
   const videoButton = document.querySelector(".preview__video-button");
   const closeButton = document.querySelector(".modal__close-button");
+  const videoPlayer = document.getElementById("videoPlayer");
 
   // Open modal
   videoButton.addEventListener("click", function () {
     modal.style.display = "block";
+
+    // Replace src attribute with video url
+    videoPlayer.src = "https://youtube.com/embed/8sXRyHI3bLw";
   });
 
   // Close Modal
   closeButton.addEventListener("click", function () {
     modal.style.display = "none";
+    videoPlayer.src = "";
+  });
+
+  // Close Model when outside is clicked
+  window.addEventListener("click", function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      videoPlayer.src = "";
+    }
   });
 });
 
